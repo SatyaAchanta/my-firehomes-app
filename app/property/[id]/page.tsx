2ee201de15007c28ec05896a5a2512d43085b1c3
Satya Achanta <personal4kartheek@gmail.com>
2024-12-18 19:29:56 -0500

    Working full property view page

--- app/property/[id]/page.tsx
+++ app/property/[id]/page.tsx
@@ -1,29 +1,90 @@
 import React from "react";
 import { getPropertById } from "@/data/properties";
 import ReactMarkdown from "react-markdown";
-import { Button } from "@/components/ui/button";
-import { ArrowLeftIcon } from "lucide-react";
-import Link from "next/link";
+import { BathIcon, BedIcon } from "lucide-react";
+import PropertyStatusBadge from "@/components/property-status-badge";
+import numeral from "numeral";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import BackButton from "./back-button";
 
 const PropertyView = async ({ params }: { params: Promise<any> }) => {
   const paramsValue = await params;
   const property = await getPropertById(paramsValue.id);
 
+  const addressLines = [
+    property.address1,
+    property.address2,
+    property.city,
+    property.zip,
+  ].filter((addressLine) => !!addressLine);
+
   return (
-    <div className="grid grid-cols-[1fr_400px]">
+    <div className="grid grid-cols-[1fr_500px]">
       <div>
-        Caraousel
+        {!!property.images && (
+          <Carousel className="w-full">
+            <CarouselContent>
+              {property.images.map((image, index) => (
+                <CarouselItem key={image}>
+                  <div className="relative h-[80vh] min-h-80">
+                    <Image
+                      src={`https://firebasestorage.googleapis.com/v0/b/my-firehomes-app.firebasestorage.app/o/${encodeURIComponent(image)}?alt=media`}
+                      alt={`Image ${index + 1}`}
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            {property.images.length > 1 && (
+              <>
+                <CarouselPrevious className="translate-x-24 size-12" />
+                <CarouselNext className="-translate-x-24 size-12" />
+              </>
+            )}
+          </Carousel>
+        )}
         <div className="property-descriptionn max-w-screen-md mx-auto py-10 px-4">
-          <Button asChild>
-            <Link href="/admin-dashboard">
-              <ArrowLeftIcon />
-              Back to Properties
-            </Link>
-          </Button>
+          <BackButton />
           <ReactMarkdown className="py-4">{property.description}</ReactMarkdown>
         </div>
       </div>
-      <div className="bg-sky-200 h-screen sticky"></div>
+      <div className="bg-sky-200 h-screen sticky top-0 grid place-items-center p-10">
+        <div className="flex flex-col gap-8 w-full">
+          <PropertyStatusBadge
+            status={property.status}
+            className="mr-auto text-base"
+          />
+          <h1 className="text-4xl font-semibold">
+            {addressLines.map((addressLine, index) => (
+              <div key={index}>
+                {addressLine}
+                {index < addressLines.length - 1 && ","}
+              </div>
+            ))}
+          </h1>
+          <h2 className="text-3xl font-light">
+            ${numeral(property.price).format("0,0")}
+          </h2>
+          <h3 className="text-2xl flex gap-10">
+            <div className="flex gap-2">
+              <BedIcon /> {property.bedrooms} Bedrooms
+            </div>
+
+            <div className="flex gap-2">
+              <BathIcon /> {property.bathrooms} Bathrooms
+            </div>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
--- components/property-status-badge.tsx
+++ components/property-status-badge.tsx
@@ -2,32 +2,35 @@ import { PropertyStatus } from "@/types";
 import { Badge } from "./ui/badge";
 
 const statusLabel = {
-    "for-sale": "For Sale",
-    draft: "Draft",
-    sold: "Sold",
-    withdrawn: "Withdrawn",
+  "for-sale": "For Sale",
+  draft: "Draft",
+  sold: "Sold",
+  withdrawn: "Withdrawn",
 };
 
 const variant: {
-    [key: string]: "primary" | "secondary" | "success" | "destructive";
+  [key: string]: "primary" | "secondary" | "success" | "destructive";
 } = {
-    "for-sale": "primary",
-    draft: "secondary",
-    sold: "success",
-    withdrawn: "destructive",
-}
+  "for-sale": "primary",
+  draft: "secondary",
+  sold: "success",
+  withdrawn: "destructive",
+};
 
-const PropertyStatusBadge = ({ status, }: {
-    status: PropertyStatus
+const PropertyStatusBadge = ({
+  status,
+  className,
+}: {
+  status: PropertyStatus;
+  className?: string;
 }) => {
+  const label = statusLabel[status];
 
-    const label = statusLabel[status];
-
-    return (
-        <Badge variant={
-            variant[status]
-        }>{label}</Badge>
-    );
+  return (
+    <Badge variant={variant[status]} className={className}>
+      {label}
+    </Badge>
+  );
 };
 
 export default PropertyStatusBadge;
